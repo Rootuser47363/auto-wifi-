6de2d9c3cf1a0d6291b8f5efca2262b95aba67b8
Root/user47363/ <82620412+Rootuser47363@users.noreply.github.com>
2023-03-02 18:34:39 -0600

    Add files via upload
    Este código es un script de PowerShell diseñado para ejecutarse en una computadora con Windows. Su objetivo es buscar y extraer las contraseñas de red WiFi guardadas en el sistema y guardarlas en un archivo de texto. El script utiliza el comando "netsh" de Windows para obtener las contraseñas, y luego las ordena alfabéticamente por el nombre de la red antes de guardarlas en el archivo de salida.
    
    El código también incluye una verificación de la versión de PowerShell y si se está ejecutando en un equipo con Windows, así como una función para preguntar al usuario si desea sobrescribir un archivo de salida existente. Finalmente, el código actualiza los permisos del archivo de salida para evitar el acceso no autorizado por parte de otros usuarios en la computadora.

---
+++ auto wifi.ps1
@@ -0,0 +1,55 @@
+# Verifica la versión de PowerShell y que se está ejecutando en un equipo con Windows
+if ($PSVersionTable.PSVersion.Major -lt 3) {
+    Write-Host "Este script requiere PowerShell 3.0 o posterior." -ForegroundColor Red
+    return
+}
+
+if (-not (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion")) {
+    Write-Host "Este script solo se puede ejecutar en un equipo con Windows." -ForegroundColor Red
+    return
+}
+
+# Cambiar el nombre del archivo de salida aquí:
+$nombre_archivo = "pass_wifi.txt"
+
+# Ejecutar el comando netsh para obtener las contraseñas y guardarlas en una variable
+$redes_wifi = netsh wlan show profile
+$contrasenas_wifi = foreach ($red in $redes_wifi) {
+    $nombre_red = $red -replace ".:\s(.*)", '$1'
+    $contrasena_red = (netsh wlan show profile name="$nombre_red" key=clear | Select-String "Contenido de la clave" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }) -join ''
+    if ($contrasena_red) {
+        "${nombre_red}: ${contrasena_red}"
+    }
+}
+
+# Ordenar las contraseñas alfabéticamente por nombre de red
+$contrasenas_wifi = $contrasenas_wifi | Sort-Object
+
+# Verificar si el archivo de salida ya existe y pedir confirmación antes de sobrescribirlo
+if (Test-Path -Path $nombre_archivo) {
+    $confirm = Read-Host "El archivo $nombre_archivo ya existe. ¿Desea sobrescribirlo? (S/N)"
+    if ($confirm -ne 'S') {
+        Write-Host "Operación cancelada." -ForegroundColor Yellow
+        return
+    } else {
+        Remove-Item -Path $nombre_archivo -ErrorAction SilentlyContinue
+    }
+}
+
+# Escribir las contraseñas ordenadas en el archivo de salida
+try {
+    $contrasenas_wifi | Out-File -FilePath $nombre_archivo -Encoding utf8 -Append
+    Write-Host "Contraseñas guardadas exitosamente en $nombre_archivo"
+} catch {
+    Write-Host "Error al escribir en el archivo: $($_.Exception.Message)" -ForegroundColor Red
+}
+
+# Agregar la recomendación de cambiar los permisos del archivo para evitar acceso no autorizado
+$archivo = Get-Item $nombre_archivo
+if ($archivo) {
+    $acl = Get-Acl $nombre_archivo
+    $ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Usuarios","ReadAndExecute","Allow")
+    $acl.SetAccessRule($ar)
+    Set-Acl $nombre_archivo $acl
+    Write-Host "Se han actualizado los permisos de $nombre_archivo para evitar acceso no autorizado."
+}
